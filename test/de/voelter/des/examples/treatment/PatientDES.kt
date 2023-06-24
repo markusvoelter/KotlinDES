878373aea2f967d1d7f3da8c1a2386ee5eab7519
Markus Voelter <voelter@acm.org>
2023-06-24 11:26:02 +0200

    rename

--- test/de/voelter/des/examples/treatment/PatientDES.kt
+++ test/de/voelter/des/examples/treatment/PatientDES.kt
@@ -29,11 +29,11 @@ class CheckNoMoreFever : AbstractEvent() {
 
 fun main() {
     // create the simulation
-    val sim = Simulation()
+    val simulation = Simulation()
 
     // register a monitor that updates the FeverPresent
     // state if the temperature reaches 38 degrees
-    sim.registerMonitor(
+    simulation.registerMonitor(
         IntIncreaseTo(
             {it.get(PatientTemperature::class)},
             38,
@@ -45,19 +45,19 @@ fun main() {
     )
 
     // initial state of the simulation
-    sim.setupState(PatientTemperature(37), PatientFever(false))
+    simulation.setupState(PatientTemperature(37), PatientFever(false))
 
     // "scripted" behavior
-    sim.updateState(PatientTemperature(38), Time(100))
-    sim.updateState(PatientTemperature(37), Time(120))
+    simulation.updateState(PatientTemperature(38), Time(100))
+    simulation.updateState(PatientTemperature(37), Time(120))
 
     // run the thing
-    sim.run()
+    simulation.run()
 
     // outputs for debugging
-    sim.stateSnapshot(Time(0)).print()
-    sim.stateSnapshot(Time(100)).print()
-    sim.stateSnapshot(Time(200)).print()
-    sim.state().printHistory()
+    simulation.stateSnapshot(Time(0)).print()
+    simulation.stateSnapshot(Time(100)).print()
+    simulation.stateSnapshot(Time(200)).print()
+    simulation.state().printHistory()
 }
 
